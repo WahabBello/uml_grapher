e79dd24eb52cc6649b20e2711db43f57d323dafd
WahabBello <abdouwahab123@gmail.com>
2022-10-24 17:13:09 +0200

    Adding few test :
    In order to have a given code voverage (=> 85),
    I add 4 more test in the sampleTestfile

--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -3,6 +3,8 @@ package fr.lernejo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class SampleTest {
 
     private final Sample sample = new Sample();
@@ -14,5 +16,37 @@ class SampleTest {
         Assertions.assertThat(result)
             .as("Addition of 2 and 2")
             .isEqualTo(4);
+    }
+    @Test
+    void fact_return_int() {
+        int result = sample.fact( 4);
+
+        boolean isInt = (result>=0);
+
+        Assertions.assertThat(isInt)
+            .as("Is it an int ?")
+            .isEqualTo(true);
+    }
+    @Test
+    void multi_2_and_3_should_return_6() {
+        int result = sample.op(Sample.Operation.MULT, 2, 3);
+
+        Assertions.assertThat(result)
+            .as("Multiplication of 2 and 3")
+            .isEqualTo(6);
+    }
+
+    @Test
+    void fact_3_return_6() {
+        int result = sample.fact( 3);
+
+        Assertions.assertThat(result)
+            .as("Facto of 3")
+            .isEqualTo(6);
+    }
+
+    @Test
+    void n_should_be_positive() {
+        assertThrows( IllegalArgumentException.class, () -> sample.fact( -3));
     }
 }
